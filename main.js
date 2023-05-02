baf73a4d4882a8cdebbccb46729c5a94c2335632
Seo-Moldes <seomoldes@gmail.com>
2023-05-02 14:09:30 +0100

    screen change update

--- main.js
+++ main.js
@@ -1,31 +1,76 @@
 
+const inputName = document.querySelector("#input");
+const buttonStart = document.querySelector("#button")
+const mainPageForm = document.querySelector("#formMainPage");
+const initGamePage = document.querySelector("#initGameDiv");
+const gameStart = document.querySelector("#divGame");
+const buttonPage3 = document.querySelector("#buttonPage3");
+const initGame = document.querySelector("#init");
+const divInitGame = document.querySelector("#divInitGame");
+const divForm = document.querySelector("#divForm");
+
+mainPageForm.classList.add("visible");
+divForm.classList.add("visible");
 
 //UPLOAD SCORES TO LOCAL STORAGE
 
-const inputName = document.querySelector("#input");
-const buttonStart = document.querySelector("#buttonStart");
+buttonStart.addEventListener("click", function (event) {
+    //evita que el formulario se envíe y cause errores al desaparecer
+    event.preventDefault();
 
-buttonStart.addEventListener("click", function() {
+    divForm.classList.remove("hidden");
+    mainPageForm.classList.remove("hidden");
+    initGamePage.classList.add("visible");
 
-let player = {Name: inputName.value, Score: 0};
+const players = {Name: inputName.value, Score: 0};
 
-if(localStorage.hasOwnProperty("player") === false) {
+if(localStorage.hasOwnProperty("players") === false ) {
 
-let arrayPlayers = [];
 
-arrayPlayers.unshift(player);
+    const arrayPlayer = [];
 
-localStorage.setItem("player", JSON.stringify(arrayPlayers));
-
-}else {
-
-   let playerLS = JSON.parse(localStorage.getItem("player"));
+    arrayPlayer.unshift(players);
     
-   playerLS.unshift(player);
+    localStorage.setItem("players", JSON.stringify(arrayPlayer));
 
-   localStorage.setItem("player", JSON.stringify(playerLS));
-    
+} else {
+
+const playersLocal = JSON.parse(localStorage.getItem("players")); 
+
+playersLocal.unshift(players);
+
+localStorage.setItem("players", JSON.stringify(playersLocal));
+
 }
 
-})
+});
+
+
+  initGame.addEventListener("click", function() {
+
+     initGamePage.classList.remove("hidden");
+     divInitGame.classList.add("visible");
+
+     let contador = 10;
+
+     
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
